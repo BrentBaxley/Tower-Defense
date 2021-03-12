00f5e08c2e12a5e131976c580ef9619a82e96be4
Brent Baxley <bbaxley@gmail.com>
2021-03-11 21:48:52 -0500

    Up to "Script Execution Order"

---
+++ Assets/Tiles/Tile.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+    [SerializeField] bool isPlaceable;
+    [SerializeField] Tower towerPrefab;
+    public bool IsPlaceable { get{ return isPlaceable; } }
+
+    GridManager gridManager;
+    Pathfinder pathfinder;
+    Vector2Int coordinates = new Vector2Int();
+
+    private void Awake() 
+    {
+        gridManager = FindObjectOfType<GridManager>();
+        pathfinder = FindObjectOfType<Pathfinder>();
+    }
+
+    private void Start() 
+    {
+        if(gridManager != null)
+        {
+            coordinates = gridManager.GetCoordinatesFromPosition(transform.position);
+
+            if(!isPlaceable)
+            {
+                gridManager.BlockNode(coordinates);
+            }
+        }
+    }
+
+    void OnMouseDown() 
+    {
+        if(gridManager.GetNode(coordinates).isWalkable && !pathfinder.WillBlockPath(coordinates))
+        {
+            bool isPlaced = towerPrefab.CreateTower(towerPrefab, transform.position);
+            isPlaceable = !isPlaced;
+            gridManager.BlockNode(coordinates);
+        }
+        
+    }
+}
